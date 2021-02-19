cd3078768d4d9d827c2433851d213e235e9064e1
YongGyunKwon <ygkwon50@gmail.com>
2021-02-19 17:22:51 +0900

    pdftoimg.py

--- Function/pdftoimg.py
+++ Function/pdftoimg.py
@@ -10,12 +10,12 @@ def pdf_to_image(pdffile,savepath):
 
     #read pdf page to png
     for i in  range(0,pagecount):
-        print(i)
+        
         page =doc.loadPage(i)
         pix= page.getPixmap()
         output= savepath+ "\\outfile_{}.jpg".format(i)
         pix.writePNG(output)
-
+        print(output)
 
 #example path
 savepath="D:\\2_CodeBase\\1_ISO26262Text\\Extract_PDF_Text\\pdffile"
@@ -23,4 +23,5 @@ savepath="D:\\2_CodeBase\\1_ISO26262Text\\Extract_PDF_Text\\pdffile"
 pdffile="D:\\2_CodeBase\\1_ISO26262Text\\Extract_PDF_Text\\pdffile\\ISO26262_guide.pdf"
 
 #For test 
-#pdf_to_image(pdffile,savepath)
+pdf_to_image(pdffile,savepath)
+
